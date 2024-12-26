00380cce3923bc7972a70d6d28c253152233adbb
Jung <jjlee030415@gmail.com>
2024-12-26 23:53:58 +0900

    ✨Feat : Django 14일차

--- app/videos/tests.py
+++ app/videos/tests.py
@@ -1,3 +1,91 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from users.models import User
+from videos.models import Video
+from django.urls import reverse # url => name을 기반으로 url 값을 불러온다.
+from rest_framework import status
+from django.core.files.uploadedfile import SimpleUploadedFile
 
-# Create your tests here.
+class VideoAPITestCase(APITestCase):
+    # 테스트 코드 케이스가 실행 되기 전 가장 먼저 실행되는 함수
+    # - 테스트 잔 데이터를 생성하는 함수
+    # - (1) 유저 생성 및 로그인 -> (2) 비디오 생성
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email='jung@gmail.com',
+            password="password123"
+        )
+
+        # 로그인
+        self.client.login(email='jung@gmail.com', password="password123")
+
+        self.video = Video.objects.create(
+            title = 'First video title',
+            link = 'http://www.tset.com',
+            user = self.user
+        )
+
+    # api/v1/video [GET] 전체 목록 조회
+    def test_video_list_get(self):
+        url = reverse('video-list') # api/v1/video
+        res = self.client.get(url)
+
+        # self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertTrue(len(res.data) > 0)
+
+        for video in res.data:
+            self.assertIn('title', video)
+
+    # api/v1/video [POST] 새로운 비디오 생성
+    def test_video_list_post(self):
+        url = reverse('video-list')
+
+        data = {
+            'title': 'My test video title',
+            'link': 'http://www.tset.com',
+            'category': 'Development',
+            'thumbnail': 'http://www.tset.com',
+            'video_file': SimpleUploadedFile('test.mp4', b'file_content', 'video/mp4'),
+            'user': self.user.pk
+        }
+
+        res = self.client.post(url, data) # VideoList [POST]
+
+        self.assertEqual(res.status_code, 201) # 201_CREATED
+        self.assertEqual(res.data['title'], 'My test video title')
+
+    # api/v1/video/{video_id} 특정 비디오 조회
+    def test_video_detail_get(self):
+        url = reverse('video-detail', kwargs={'pk':self.video.pk})
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+    
+    # api/v1/video/{video_id} 특정 비디오 압데이트
+    def test_video_detail_put(self):
+        url = reverse('video-detail', kwargs={'pk':self.video.pk})
+
+        data = {
+            'title': 'Updated Video title',
+            'link': 'http://www.tset.com',
+            'category': 'Development',
+            'thumbnail': 'http://www.tset.com',
+            'video_file': SimpleUploadedFile('test.mp4', b'file_content', 'video/mp4'),
+            'user': self.user.pk
+        }
+
+        res = self.client.put(url, data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.data['title'], 'Updated Video title')
+
+    # api/v1/video/{video_id} 특정 비디오 삭제
+    def test_video_detail_delete(self):
+        url = reverse('video-detail', kwargs={'pk':self.video.pk})
+
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, 204)
+
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 404)
--- app/videos/views.py
+++ app/videos/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Video
+from .serializers import  VideoListSerializer, VideoDetailSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 
-# Create your views here.
+# Video와 관련된 REST API
+# 1.VideoList
+# api/v1/video
+# [GET]: 전체 비디오 목록 조회
+# [POST]: 새로운 비디오 생성
+# [PUT], [DELETE]: X
+class VideoList(APIView):
+    def get(self, request):
+        videos = Video.objects.all() # objects => QuerySet[Video, Video, Video, Video ...]
+
+        # objects -> Json (직렬화)
+        serializer = VideoListSerializer(videos, many=True) # QuerySet 데이터를 불러올 때 객체들이 하나가 아니라 여러개여서 many=True가 들어감
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        user_data = request.data # Json -> object (역직렬화)
+
+        serializer = VideoListSerializer(data=user_data)
+
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+# 2.VideoDetail
+# api/v1/video/{video_id}
+# [GET]: 특정 비디오 조회
+# [POST]: X
+# [PUT]: 특정 비디오 업데이트
+# [DELETE]: 특정 비디오 삭제
+# 디테일은 유저로부터 pk라는 값을 받아옴 그리고 어떤 비디오를 삭제할지 업데이트 할지 모르기 때문에 pk가 들어감
+class VideoDetail(APIView):
+    def get(self, request, pk):
+        try:    
+            video_obj = Video.objects.get(pk=pk)
+
+        except Video.DoesNotExist:
+            raise NotFound
+        
+        serializer = VideoDetailSerializer(video_obj)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        video_obj = Video.objects.get(pk=pk)
+        user_data = request.data
+
+        serializer = VideoDetailSerializer(video_obj, user_data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        video_obj = Video.objects.get(pk=pk)
+        video_obj.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
